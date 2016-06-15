06eacef8dd256b48bea2ddf630f8f70b18e9967e
Nate Ruiz <nateruiz1997@yahoo.com>
2016-06-15 10:23:19 -0400

    Merge remote-tracking branch 'ihoegen/master'
    added an on change

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,7 +2,9 @@ function loadChapters() {
 
   var chapter = document.getElementById("chapter");
   var chapterList = document.createElement("select");
+  chapterList.id = 'chapter-dropdown'
   chapterList.className = "form-control";
+  chapterList.onchange= verseCount();
   chapter.appendChild(chapterList);
   //Create and append the options
   for (var i = 1; i <= chapters.length; i++) {
@@ -24,5 +26,12 @@ function loadVerses() {
     option.value = "Verse " + i;
     option.text = "Verse " + i;
     verseList.appendChild(option);
+
   }
+}
+function verseCount(){
+  var selected = document.getElementById("chapter-dropdown").value;
+  console.log(chapters[-1].chapter.length);
+
+
 }
