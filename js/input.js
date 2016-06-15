3ea9a40f3bd3c968a41be9b761f54c9ecf187558
Ian Hoegen <ianhoegen@gmail.com>
2016-06-15 10:35:14 -0400

    Used regex to filter

--- js/input.js
+++ js/input.js
@@ -9,7 +9,6 @@ $.ajax({
       bookName.push(book);
         for(var chapter in text[book]) {
           var currentChapter = {};
-          console.log(chapter);
           currentChapter.chapter = [];
             for (var verse in text[book][chapter]){
               currentChapter.chapter.push(verse);
@@ -17,6 +16,8 @@ $.ajax({
             chapters.push(currentChapter);
         }
     }
+    console.log(chapters);
+
     bibleText = text;
     parseText(bibleText);
     document.title = bookName[0];
@@ -45,8 +46,12 @@ function strongMapping(text) {
 function parseText(text) {
   for(book in text){
     for(chapter in text[book]){
+      $('#bible').append('<h4> Chapter' + chapter + '</h4>');
       for(verse in text[book][chapter]){
-        console.log(verse);
+        var currentString = text[book][chapter][verse];
+        var re = /{[^>]*}|(\w+)|[-]|[[]/g;
+        currentString = currentString.replace(re, '');
+        $('#bible').append('<p><strong>' + verse  + '</strong> ' + currentString + '</p>');
       }
     }
   }
