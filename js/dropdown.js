d5a543790f2789df9c7c8c19381c870048b95665
Jay Scott <jay.a.scottjr@biola.edu>
2016-06-15 15:18:41 -0300

    Up to date

---
+++ js/dropdown.js
@@ -0,0 +1,47 @@
+function loadChapters() {
+
+  var chapter = document.getElementById("chapter");
+  var chapterList = document.createElement("select");
+  chapterList.id = 'chapter-dropdown'
+  chapterList.className = "form-control";
+  chapter.appendChild(chapterList);
+  //Create and append the options
+  for (var i = 1; i <= chapters.length; i++) {
+    var option = document.createElement("option");
+    option.value = i;
+    option.text = "Chapter " + i;
+    chapterList.appendChild(option);
+  }
+  chapterList.onchange = verseCount;
+  var verse = document.getElementById("verse");
+  var verseList = document.createElement("select");
+  verseList.className = "form-control"
+  verse.appendChild(verseList);
+  verseList.id="verseList";
+  verseCount();
+}
+
+
+function loadVerses(total) {
+  var verse = document.getElementById("verse");
+  var verseList = document.getElementById("verseList");
+  //Create and append the options
+  for (var i = 1; i <= total; i++) {
+    var option = document.createElement("option");
+    option.value = "Verse " + i;
+    option.text = "Verse " + i;
+    verseList.appendChild(option);
+
+  }
+}
+function verseCount(){
+  var selected = document.getElementById("chapter-dropdown").value;
+   var total=(chapters[selected-1].chapter.length);
+   var verseList = document.getElementById("verseList");
+   while(verseList.firstChild!=null){
+   verseList.removeChild(verseList.firstChild);
+ }
+   loadVerses(total);
+
+
+}
