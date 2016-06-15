c321f6630e3b5fbed813d87c8c870eeaf7e3b81f
Sterling (Jay) Scott <jay.a.scottjr@biola.edu>
2016-06-15 15:33:56 -0300

    added new functions

--- js/input.js
+++ js/input.js
@@ -69,3 +69,21 @@ function parseText(text) {
     }
   }
 }
+
+
+function getSelectedText() {
+    var text = "";
+    if (typeof window.getSelection != "undefined") {
+        text = window.getSelection().toString();
+    } else if (typeof document.selection != "undefined" && document.selection.type == "Text") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+function doSomethingWithSelectedText() {
+    var selectedText = getSelectedText();
+    if (selectedText) {
+		console.log(selectedText.id)
+    }
+}
